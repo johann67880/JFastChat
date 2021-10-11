749f59f0cff3becb51e2ad640ab10dfd81439d1f
johaljicar@hotmail.com <johaljicar@hotmail.com>
2021-10-10 21:04:01 -0500

    Adding field validations

--- app/src/main/java/com/johaljicar/jfastchat/MainActivity.kt
+++ app/src/main/java/com/johaljicar/jfastchat/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.ArrayAdapter
 import com.johaljicar.jfastchat.databinding.ActivityMainBinding
 import android.content.Intent
 import android.net.Uri
+import android.widget.Toast
 import com.google.android.gms.ads.AdRequest
 
 class MainActivity : AppCompatActivity() {
@@ -33,6 +34,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun openWhatsApp(view: View) {
+
+        if(_binding.autoCompleteTextView.text.toString().isEmpty()) {
+            Toast.makeText(this, R.string.emptyCountry, Toast.LENGTH_LONG).show()
+            return;
+        }
+
+        if(_binding.txtCellphoneNumber.text.toString().isEmpty()) {
+            Toast.makeText(this, R.string.emptyNumber, Toast.LENGTH_LONG).show()
+            return;
+        }
+
         val countryCode = _binding.autoCompleteTextView.text
             .split("+")[1]
             .replace(")", "")
